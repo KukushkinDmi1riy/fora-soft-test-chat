80511a44a990db8a66dd4411080784fed7bf5c6b
KukushkinDmi1riy <777Dmitrij@gmail.com>
2021-05-18 10:48:59 +0300

    new port

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,62 @@
-import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+
+import socket from './socket';
+
+import reducer from './reducer';
+import JoinBlock from './components/joinBlock/JoinBlock';
+import Chat from './components/chat/Chat';
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [state, dispatch] = React.useReducer(reducer, {
+    joined: false,
+    roomId: null,
+    userName: null,
+    users: [],
+    messages: [],
+  });
+
+  const onLogin = async (obj) => {
+    dispatch({
+      type: 'JOINED',
+      payload: obj,
+    });
+    socket.emit('ROOM:JOIN', obj);
+    const { data } = await axios.get(`/rooms/${obj.roomId}`);
+    dispatch({
+      type: 'SET_DATA',
+      payload: data,
+    });
+  };
+
+  const setUsers = (users) => {
+    dispatch({
+      type: 'SET_USERS',
+      payload: users,
+    });
+  };
+
+  const addMessage = (message) => {
+    dispatch({
+      type: 'NEW_MESSAGE',
+      payload: message,
+    });
+  };
 
   React.useEffect(() => {
-    fetch('/room')
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+    socket.on('ROOM:SET_USERS', setUsers);
+    socket.on('ROOM:NEW_MESSAGE', addMessage);
   }, []);
 
+  window.socket = socket;
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <div className="wrapper">
+      {!state.joined ? (
+        <JoinBlock onLogin={onLogin} />
+      ) : (
+        <Chat {...state} onAddMessage={addMessage} />
+      )}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,75 @@
 const path = require('path');
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
-
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server,{cors:{origin:"*"}});
 
+const PORT = process.env.PORT || 3002;
+
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get('/room', (req, res) => {
-  res.json({ message: 'Hi man!' });
+const rooms = new Map();
+
+app.get('/rooms/:id', (req, res) => {
+  const { id: roomId } = req.params;
+  const obj = rooms.has(roomId)
+    ? {
+        users: [...rooms.get(roomId).get('users').values()],
+        messages: [...rooms.get(roomId).get('messages').values()],
+      }
+    : { users: [], messages: [] };
+  res.json(obj);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.post('/rooms', (req, res) => {
+  const { roomId, userName } = req.body;
+  if (!rooms.has(roomId)) {
+    rooms.set(
+      roomId,
+      new Map([
+        ['users', new Map()],
+        ['messages', []],
+      ]),
+    );
+  }
+  res.send();
+});
+
+io.on('connection', (socket) => {
+  socket.on('ROOM:JOIN', ({ roomId, userName }) => {
+    socket.join(roomId);
+    rooms.get(roomId).get('users').set(socket.id, userName);
+    const users = [...rooms.get(roomId).get('users').values()];
+    socket.broadcast.to(roomId).emit('ROOM:SET_USERS', users);
+  });
+
+  socket.on('ROOM:NEW_MESSAGE', ({ roomId, userName, text }) => {
+    const obj = {
+      userName,
+      text,
+    };
+    rooms.get(roomId).get('messages').push(obj);
+    socket.broadcast.to(roomId).emit('ROOM:NEW_MESSAGE', obj);
+  });
+
+  socket.on('disconnect', () => {
+    rooms.forEach((value, roomId) => {
+      if (value.get('users').delete(socket.id)) {
+        const users = [...value.get('users').values()];
+        socket.broadcast.to(roomId).emit('ROOM:SET_USERS', users);
+      }
+    });
+  });
+
+  console.log('user connected', socket.id);
+});
+
+
+server.listen(PORT, (err) => {
+  if (err) {
+    throw Error(err);
+  }
+  console.log('Сервер запущен!');
 });
